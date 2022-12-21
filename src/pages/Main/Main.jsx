baf55cd8dda571922dd0f7e8eee8553cdbd96508
Artyom <artyom-first@yandex.ru>
2022-12-21 22:45:10 +0500

    feat: создать теги и добавить css-файлы

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Main.css';
+import bannerImage from '../../images/main__banner.svg';
 
 const Main = ({ loggedIn }) => {
   return (
     <main className='main'>
       <section className='banner'>
-
+        <img className='banner__image' src={bannerImage} alt='питомцы' />
+        <h1 className='banner__title'>Помогаем тем, кто в этом нуждается</h1>
+        <div className='banner__line'></div>
+        <p className='banner__subtitle'>Лапки помощи - это проект помощи приютам для животных.</p>
+        <button className='banner__button' type='button'>Хочу помогать</button>
       </section>
       <section className='promo'>
 
