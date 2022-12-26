396794fbb63674976f664384a125c8058442b6b2
Artyom <artyom-first@yandex.ru>
2022-12-26 20:38:57 +0500

    refactor: преобразовать элементы faq__card в элементы detalis/summary

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -28,32 +28,40 @@ const Main = () => {
         <h2 className='faq__title'>FAQ</h2>
         <ul className='faq__list'>
           <li className='faq__card'>
-            <div className='faq__card-row'>
-              <h3 className='faq__card-title'>Как пожертвовать деньги приюту?</h3>
-              <img className='faq__card-icon' src={faqMinus} alt='свернуть' />
-            </div>
-            <p className='faq__card-text'>Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”</p>
+            <details className='faq__detalis' open>
+              <summary className='faq__card-row'>
+                <h3 className='faq__card-title'>Как пожертвовать деньги приюту?</h3>
+                <div className='faq__card-icon' />
+              </summary>
+              <p className='faq__card-text'>Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”</p>
+            </details>
           </li>
           <li className='faq__card'>
-            <div className='faq__card-row'>
-              <h3 className='faq__card-title'>Как взять питомца домой?</h3>
-              <img className='faq__card-icon' src={faqPlus} alt='развернуть' />
-            </div>
-            <p className='faq__card-text display_none'>Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”</p>
+            <details className='faq__detalis'>
+              <summary className='faq__card-row'>
+                <h3 className='faq__card-title'>Как взять питомца домой?</h3>
+                <div className='faq__card-icon' src={faqPlus} alt='развернуть' />
+              </summary>
+              <p className='faq__card-text'>Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”</p>
+            </details>
           </li>
           <li className='faq__card'>
-            <div className='faq__card-row'>
-              <h3 className='faq__card-title'>Как зарегистрировать приют?</h3>
-              <img className='faq__card-icon' src={faqPlus} alt='развернуть' />
-            </div>
-            <p className='faq__card-text display_none'>Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”</p>
+            <details className='faq__detalis'>
+              <summary className='faq__card-row'>
+                <h3 className='faq__card-title'>Как зарегистрировать приют?</h3>
+                <div className='faq__card-icon' src={faqPlus} alt='развернуть' />
+              </summary>
+              <p className='faq__card-text'>Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”</p>
+            </details>
           </li>
           <li className='faq__card'>
-            <div className='faq__card-row'>
-              <h3 className='faq__card-title'>Какая минимальная сумма пожертвования?</h3>
-              <img className='faq__card-icon' src={faqPlus} alt='развернуть' />
-            </div>
-            <p className='faq__card-text display_none'>Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”</p>
+            <details className='faq__detalis'>
+              <summary className='faq__card-row'>
+                <h3 className='faq__card-title'>Какая минимальная сумма пожертвования?</h3>
+                <div className='faq__card-icon' src={faqPlus} alt='развернуть' />
+              </summary>
+              <p className='faq__card-text'>Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”</p>
+            </details>
           </li>
         </ul>
       </section>
