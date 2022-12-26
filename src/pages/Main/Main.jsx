438cc88cad7dffb30e7737af84fd96ea72c5ec7e
Artyom <artyom-first@yandex.ru>
2022-12-26 18:23:10 +0500

    refactor: преобразовать элементы ahelter-cad в список

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -15,32 +15,32 @@ const Main = () => {
       </section>
       <section className='shelters-on-main'>
         <h2 className='shelters-on-main__title'>Приюты, которым очень нужна помощь</h2>
-        <div className='shelters-on-main__shelters-container'>
-          <div className='shelter-card'>
+        <ul className='shelters-on-main__shelters-container'>
+          <li className='shelter-card'>
             <img className='shelter-card__logo' src={shelterLogo} alt='лого приюта' />
             <h3 className='shelter-card__title'>Приют Бирюлево</h3>
             <p className='shelter-card__address'>Востряковский пр-д, 10А, Москва, Россия</p>
             <p className='shelter-card__working-hours'>Часы работы: <span className='shelter-card__hours'>10:00 - 18:00</span></p>
             <Link className='shelter-card__link' to='/shelters'>Читать подробнее</Link>
             <img className='shelter-card__paw' src={paw} alt='ЧП' />
-          </div>
-          <div className='shelter-card'>
+          </li>
+          <li className='shelter-card'>
             <img className='shelter-card__logo' src={shelterLogo} alt='лого приюта' />
             <h3 className='shelter-card__title'>Приют Бирюлево</h3>
             <p className='shelter-card__address'>Востряковский пр-д, 10А, Москва, Россия</p>
             <p className='shelter-card__working-hours'>Часы работы: <span className='shelter-card__hours'>10:00 - 18:00</span></p>
             <Link className='shelter-card__link' to='/shelters'>Читать подробнее</Link>
             <img className='shelter-card__paw' src={paw} alt='ЧП' />
-          </div>
-          <div className='shelter-card'>
+          </li>
+          <li className='shelter-card'>
             <img className='shelter-card__logo' src={shelterLogo} alt='лого приюта' />
             <h3 className='shelter-card__title'>Приют Бирюлево</h3>
             <p className='shelter-card__address'>Востряковский пр-д, 10А, Москва, Россия</p>
             <p className='shelter-card__working-hours'>Часы работы: <span className='shelter-card__hours'>10:00 - 18:00</span></p>
             <Link className='shelter-card__link' to='/shelters'>Читать подробнее</Link>
             <img className='shelter-card__paw' src={paw} alt='ЧП' />
-          </div>
-        </div>
+          </li>
+        </ul>
         <Link className='shelters-on-main__link margin-left_auto' to='/map'>Смотреть на карте</Link>
         <div className='shelters-on-main__text-container'>
           <h3 className='shelters-on-main__text-title'>Вы владелец приюта?</h3>
