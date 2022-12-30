6963295f50b1f5c091f7cb46386fa26c9e8d14a3
Artyom <artyom-first@yandex.ru>
2022-12-30 23:29:28 +0500

    feat: прописать теги и подключить css-файлы для Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css';
+import vkIcon from '../../images/footer__icon_vk.svg';
+import telegramIcon from '../../images/footer__icon_telegram.svg';
 
 const Footer = () => {
   return (
-    <header className='header'>
-
-    </header>
+    <footer className='footer'>
+      <div className='footer__title-container'>
+        <p className='footer__title'>Лапки помощи</p>
+        <p className='footer__subtitle'>Сервис для помощи приютам </p>
+      </div>
+      <div className='footer__text-links'>
+        <Link className='footer__link' to='/map'>Карта приютов</Link>
+        <Link className='footer__link' to='/news'>Новости</Link>
+        <Link className='footer__link' to='/papers'>Полезные статьи</Link>
+        <Link className='footer__link' to='/about-us'>О компании</Link>
+        <Link className='footer__link' to='/faq'>FAQ</Link>
+      </div>
+      <div className='footer__icons'>
+        <Link className='footer__icon-link' to=''>
+          <img className='footer__icon' src={vkIcon} alt='ВК' />
+        </Link>
+        <Link className='footer__icon-link' to=''>
+          <img className='footer__icon' src={telegramIcon} alt='телеграм' />
+        </Link>
+      </div>
+    </footer>
   );
 }
 
