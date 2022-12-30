af1c607cc2c998d2a7495f794720a83aa9225886
Elvira <sabirova.elvira.gam@gmail.com>
2022-12-30 07:36:56 +0500

    feat: добавить список приютов на карту

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,11 +1,35 @@
+import './MapPage.css';
 import { Map, Placemark, YMaps } from '@pbe/react-yandex-maps';
 import pawIcon from '../images/icons/ic_paw.svg';
 
 const MapPage = () => (
   <YMaps>
-    <div>
-      <img src={pawIcon} alt=""/>
-      <Map defaultState={{ center: [55.75, 37.57], zoom: 9, controls: [] }}>
+    <div className='container container__flex_row'>
+      <ul className="shelters-list">
+        <li className="shelters-list__item shelter">
+          <h3 className="shelter__title">
+            Приют Бирюлево
+          </h3>
+          <p className="shelter__address">
+            Востряковский пр-д, 10А,
+            Москва, Россия.
+          </p>
+          <p className="shelter__time"><span className="text-accent">Часы работы: </span>10:00 -
+            18:00</p>
+        </li>
+        <li className="shelters-list__item shelter">
+          <h3 className="shelter__title">
+            Приют Бирюлево
+          </h3>
+          <p className="shelter__address">
+            Востряковский пр-д, 10А,
+            Москва, Россия.
+          </p>
+          <p className="shelter__time"><span className="text-accent">Часы работы: </span>10:00 -
+            18:00</p>
+        </li>
+      </ul>
+      <Map height={1032} width={914} defaultState={{ center: [55.75, 37.57], zoom: 9, controls: [] }}>
         <Placemark geometry={[55.684758, 37.738521]} options={
           {
             iconLayout: 'default#image',
@@ -27,42 +51,48 @@ const MapPage = () => (
                        iconLayout: 'default#image',
                        iconImageHref: pawIcon,
                        iconImageSize: [20, 20],
-                       iconImageOffset: [-20, -20],} }/>
+                       iconImageOffset: [-20, -20],
+                     }}/>
         <Placemark geometry={[55.744658, 37.898571]}
                    options={
                      {
                        iconLayout: 'default#image',
                        iconImageHref: pawIcon,
                        iconImageSize: [20, 20],
-                       iconImageOffset: [-20, -20],} }/>
+                       iconImageOffset: [-20, -20],
+                     }}/>
         <Placemark geometry={[55.694758, 37.728521]}
                    options={
                      {
                        iconLayout: 'default#image',
                        iconImageHref: pawIcon,
                        iconImageSize: [20, 20],
-                       iconImageOffset: [-20, -20],} }/>
+                       iconImageOffset: [-20, -20],
+                     }}/>
         <Placemark geometry={[55.684658, 37.738571]}
                    options={
                      {
                        iconLayout: 'default#image',
                        iconImageHref: pawIcon,
                        iconImageSize: [20, 20],
-                       iconImageOffset: [-20, -20],} }/>
+                       iconImageOffset: [-20, -20],
+                     }}/>
         <Placemark geometry={[52.684758, 30.738521]}
                    options={
                      {
                        iconLayout: 'default#image',
                        iconImageHref: pawIcon,
                        iconImageSize: [20, 20],
-                       iconImageOffset: [-20, -20],} }/>
+                       iconImageOffset: [-20, -20],
+                     }}/>
         <Placemark geometry={[51.684758, 39.738521]}
                    options={
                      {
                        iconLayout: 'default#image',
                        iconImageHref: pawIcon,
                        iconImageSize: [20, 20],
-                       iconImageOffset: [-20, -20],} }/>
+                       iconImageOffset: [-20, -20],
+                     }}/>
       </Map>
     </div>
   </YMaps>
