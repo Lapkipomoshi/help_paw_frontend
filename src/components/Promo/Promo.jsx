e4085c8393f40fb67e53c80545e1b3d1e60585c1
Artyom <artyom-first@yandex.ru>
2023-01-08 13:06:36 +0500

    feat: разделить фотографии и сделать их вертикальными in section promo in MainPage

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Promo.css';
-import promoPhoto from '../../images/main__promo.svg';
+import promoPhotoLeft from '../../images/main__promo_position_left.jpg';
+import promoPhotoRight from '../../images/main__promo_position_right.jpg';
 
 const Promo = () => {
   return (
     <section className='promo'>
-      <img className='promo__photo' src={promoPhoto} alt='наши любимцы' />
+      <div className='promo__photo-container'>
+        <img className='promo__photo promo__photo_position_left' src={promoPhotoLeft} alt='наши любимцы' />
+        <img className='promo__photo promo__photo_position_right' src={promoPhotoRight} alt='наши любимцы' />
+      </div>
       <div className='promo__text-container'>
         <h2 className='section-title'>О нас</h2>
         <p className='promo__text'>Наш проект “Лапка помощи” призван помочь приютам</p>
