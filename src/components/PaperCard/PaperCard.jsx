ffae7a1a42e9fe60c4fcd0e10fff1c9a7c262aad
Artyom <artyom-first@yandex.ru>
2023-01-09 18:51:42 +0500

    refacotr: на странице PapersPage вынести карточки в отдельные компоненты

--- src/components/PaperCard/PaperCard.jsx
+++ src/components/PaperCard/PaperCard.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './PaperCard.css';
 
-const PaperCard = ({ photo }) => {
+const PaperCard = ({ photo, text }) => {
   return (
     <Link className='paper-card' to='/papers/1'>
       <img className='paper-card__image' src={photo} alt='фото' />
-      <p className='paper-card__text'>Животное из приюта: что надо знать перед тем, как взять его в дом?</p>
+      <p className='paper-card__text'>{text}</p>
     </Link>
   );
 }
--- src/components/PapersOnMain/PapersOnMain.jsx
+++ src/components/PapersOnMain/PapersOnMain.jsx
@@ -11,13 +11,19 @@ const PapersOnMain = () => {
       <h2 className='section-title'>Полезные статьи</h2>
       <ul className='papers-on-main__flex-container'>
         <li className='papers-on-main__flex-element'>
-          <PaperCard photo={paperPhoto1} />
+          <PaperCard
+            photo={paperPhoto1}
+            text={'Животное из приюта: что надо знать перед тем, как взять его в дом?'} />
         </li>
         <li className='papers-on-main__flex-element'>
-          <PaperCard photo={paperPhoto2} />
+          <PaperCard
+            photo={paperPhoto2}
+            text={'Как отучить собаку прыгать на вас при встрече'} />
         </li>
         <li className='papers-on-main__flex-element'>
-          <PaperCard photo={paperPhoto3} />
+          <PaperCard
+            photo={paperPhoto3}
+            text={'Современные кинологи не применяют наказание. Почему?'} />
         </li>
       </ul>
     </section>
