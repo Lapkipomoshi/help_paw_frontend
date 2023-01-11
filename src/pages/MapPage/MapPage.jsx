7ac29ace42a2d1acf2bd13cebaf6c6d9fbe3b8fe
Elvira <sabirova.elvira.gam@gmail.com>
2023-01-11 16:32:38 +0500

    feat: добавить стили для списка приютов на карте

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,37 +1,72 @@
 import './MapPage.css';
 import { Map, Placemark, YMaps } from '@pbe/react-yandex-maps';
 import pawIcon from '../../images/icons/ic_paw.svg';
+import websiteIcon from '../../images/icons/ic_website.svg'
 
 const MapPage = () => {
   return (<YMaps>
     <div className="container container__flex_row">
-      <ul className="shelters-list">
-        <li className="shelters-list__item shelter">
-          <h3 className="shelter__title">
-            Приют Бирюлево
-          </h3>
-          <p className="shelter__address">
-            Востряковский пр-д, 10А,
-            Москва, Россия.
-          </p>
-          <p className="shelter__time"><span className="text-accent">Часы работы: </span>10:00 -
-            18:00</p>
-        </li>
-        <li className="shelters-list__item shelter">
-          <h3 className="shelter__title">
-            Приют Бирюлево
-          </h3>
-          <p className="shelter__address">
-            Востряковский пр-д, 10А,
-            Москва, Россия.
-          </p>
-          <p className="shelter__time"><span className="text-accent">Часы работы: </span>10:00 -
-            18:00</p>
-        </li>
-      </ul>
-      <Map height={1032} width={914}
+      <Map style={{ position: 'relative', width: '100%', height: 914 }}
            defaultState={{ center: [55.75, 37.57], zoom: 9, controls: [] }}
            modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}>
+        <ul className="shelters-list">
+          <li className="shelters-list__item shelter">
+            <div className='shelter__inf'>
+              <h3 className="shelter__title">
+                Приют Бирюлево
+              </h3>
+              <p className="shelter__address">
+                Востряковский пр-д, 10А,
+                Москва, Россия.
+              </p>
+              <p className="shelter__time"><span className="text-accent">Часы работы: </span>10:00 -
+                18:00</p>
+            </div>
+            <img src={websiteIcon} alt='website'/>
+          </li>
+          <li className="shelters-list__item shelter">
+            <div className='shelter__inf'>
+              <h3 className="shelter__title">
+                Приют Бирюлево
+              </h3>
+              <p className="shelter__address">
+                Востряковский пр-д, 10А,
+                Москва, Россия.
+              </p>
+              <p className="shelter__time"><span className="text-accent">Часы работы: </span>10:00 -
+                18:00</p>
+            </div>
+            <img src={websiteIcon} alt='website'/>
+          </li>
+          <li className="shelters-list__item shelter">
+            <div className='shelter__inf'>
+              <h3 className="shelter__title">
+                Приют Бирюлево
+              </h3>
+              <p className="shelter__address">
+                Востряковский пр-д, 10А,
+                Москва, Россия.
+              </p>
+              <p className="shelter__time"><span className="text-accent">Часы работы: </span>10:00 -
+                18:00</p>
+            </div>
+            <img src={websiteIcon} alt='website'/>
+          </li>
+          <li className="shelters-list__item shelter">
+            <div className='shelter__inf'>
+              <h3 className="shelter__title">
+                Приют Бирюлево
+              </h3>
+              <p className="shelter__address">
+                Востряковский пр-д, 10А,
+                Москва, Россия.
+              </p>
+              <p className="shelter__time"><span className="text-accent">Часы работы: </span>10:00 -
+                18:00</p>
+            </div>
+            <img src={websiteIcon} alt='website'/>
+          </li>
+        </ul>
         <Placemark geometry={[55.684758, 37.738521]}
                    properties={
                      {
