6f843be7aa04094b31f6fec587a014e39fa605e9
Artyom <artyom-first@yandex.ru>
2023-01-12 20:34:30 +0500

    fix: убрать полоску у image error-404

--- src/pages/ShelterPage/ShelterPage.jsx
+++ src/pages/ShelterPage/ShelterPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route, Outlet, NavLink } from 'react-router-dom';
 import './ShelterPage.css';
 
 const ShelterPage = () => {
   return (
     <main className='main'>
-      
+      <section className='shelter-menu'>
+        <menu className='shelter-menu__menu'>
+
+        </menu>
+      </section>
     </main>
   );
 }
