245656a74c51f4e22a975edfa12a29e5607baf11
Artyom <artyom-first@yandex.ru>
2023-01-12 23:42:23 +0500

    refactor: заменить тег ul на nav in ShelterPage

--- src/pages/ShelterPage/ShelterPage.jsx
+++ src/pages/ShelterPage/ShelterPage.jsx
@@ -6,23 +6,13 @@ const ShelterPage = () => {
   return (
     <main className='main'>
       <section className='shelter-menu'>
-        <ul className="shelter-menu__menu">
-          <li>
-            <NavLink to='about' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>О приюте</NavLink>
-          </li>
-          <li>
-            <NavLink to='how-to-help' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>Как помочь</NavLink>
-          </li>
-          <li>
-            <NavLink to='news' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>Новости</NavLink>
-          </li>
-          <li>
-            <NavLink to='pets' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>Питомцы</NavLink>
-          </li>
-          <li>
-            <NavLink to='vacancies' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>Вакансии</NavLink>
-          </li>
-        </ul>
+        <nav className="shelter-menu__menu">
+          <NavLink to='about' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>О приюте</NavLink>
+          <NavLink to='how-to-help' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>Как помочь</NavLink>
+          <NavLink to='news' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>Новости</NavLink>
+          <NavLink to='pets' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>Питомцы</NavLink>
+          <NavLink to='vacancies' className={({isActive}) => `shelter-menu__link ${isActive ? 'shelter-menu__link_active' : ''}`}>Вакансии</NavLink>
+        </nav>
       </section>
       <Outlet />
     </main>
