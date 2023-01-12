fbf260e2db822d9c5446144fa20ce354c6c67853
Elvira <sabirova.elvira.gam@gmail.com>
2023-01-12 07:34:56 +0500

    feat: добавить стили для балуна на карте

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -4,7 +4,9 @@ import pawIcon from '../../images/icons/ic_paw.svg';
 import websiteIcon from '../../images/icons/ic_website.svg';
 import { Link } from 'react-router-dom';
 
+
 const MapPage = () => {
+
   return (<YMaps>
     <div className="container container__flex_row">
       <Map style={{ position: 'relative', width: '100%', height: 914 }}
@@ -135,19 +137,34 @@ const MapPage = () => {
         <Placemark geometry={[55.684758, 37.738521]}
                    properties={
                      {
-                       balloonContentHeader: 'Paw help',
-                       balloonContentBody: 'nsj23232',
-                       balloonContentFooter: 'cdsbhcjs help',
+                       balloonContentHeader: '              <h3 class="shelter__title shelter__title_balloon">\n' +
+                         '                Приют Бирюлево\n' +
+                         '              </h3>\n',
+                       balloonContentBody: '<p class="shelter__address shelter__address_balloon">Востряковский пр-д, 10А, Москва, Россия.</p>',
+                       balloonContentFooter: '              <p class="shelter__time shelter__time_balloon"><span\n' +
+                         '                class="shelter__time-heading shelter__time-heading_balloon">Часы работы: </span>10:00 -\n' +
+                         '                18:00</p>',
                      }
                    }
                    options={
                      {
                        iconLayout: 'default#image',
                        iconImageHref: pawIcon,
-                       iconImageSize: [40, 40],
+                       iconImageSize: [20, 20],
                        iconImageOffset: [-20, -20],
                      }}/>
         <Placemark geometry={[55.644658, 37.798571]}
+                   properties={
+                     {
+                       balloonContentHeader: '              <h3 class="shelter__title shelter__title_balloon">\n' +
+                         '                Приют Бирюлево\n' +
+                         '              </h3>\n',
+                       balloonContentBody: '<p class="shelter__address shelter__address_balloon">Востряковский пр-д, 10А, Москва, Россия.</p>',
+                       balloonContentFooter: '              <p class="shelter__time shelter__time_balloon"><span\n' +
+                         '                class="shelter__time-heading shelter__time-heading_balloon">Часы работы: </span>10:00 -\n' +
+                         '                18:00</p>',
+                     }
+                   }
                    options={
                      {
                        iconLayout: 'default#image',
@@ -156,6 +173,17 @@ const MapPage = () => {
                        iconImageOffset: [-20, -20],
                      }}/>
         <Placemark geometry={[55.644658, 37.638571]}
+                   properties={
+                     {
+                       balloonContentHeader: '              <h3 class="shelter__title shelter__title_balloon">\n' +
+                         '                Приют Бирюлево\n' +
+                         '              </h3>\n',
+                       balloonContentBody: '<p class="shelter__address shelter__address_balloon">Востряковский пр-д, 10А, Москва, Россия.</p>',
+                       balloonContentFooter: '              <p class="shelter__time shelter__time_balloon"><span\n' +
+                         '                class="shelter__time-heading shelter__time-heading_balloon">Часы работы: </span>10:00 -\n' +
+                         '                18:00</p>',
+                     }
+                   }
                    options={
                      {
                        iconLayout: 'default#image',
@@ -164,6 +192,17 @@ const MapPage = () => {
                        iconImageOffset: [-20, -20],
                      }}/>
         <Placemark geometry={[55.744658, 37.898571]}
+                   properties={
+                     {
+                       balloonContentHeader: '              <h3 class="shelter__title shelter__title_balloon">\n' +
+                         '                Приют Бирюлево\n' +
+                         '              </h3>\n',
+                       balloonContentBody: '<p class="shelter__address shelter__address_balloon">Востряковский пр-д, 10А, Москва, Россия.</p>',
+                       balloonContentFooter: '              <p class="shelter__time shelter__time_balloon"><span\n' +
+                         '                class="shelter__time-heading shelter__time-heading_balloon">Часы работы: </span>10:00 -\n' +
+                         '                18:00</p>',
+                     }
+                   }
                    options={
                      {
                        iconLayout: 'default#image',
@@ -172,6 +211,17 @@ const MapPage = () => {
                        iconImageOffset: [-20, -20],
                      }}/>
         <Placemark geometry={[55.694758, 37.728521]}
+                   properties={
+                     {
+                       balloonContentHeader: '              <h3 class="shelter__title shelter__title_balloon">\n' +
+                         '                Приют Бирюлево\n' +
+                         '              </h3>\n',
+                       balloonContentBody: '<p class="shelter__address shelter__address_balloon">Востряковский пр-д, 10А, Москва, Россия.</p>',
+                       balloonContentFooter: '              <p class="shelter__time shelter__time_balloon"><span\n' +
+                         '                class="shelter__time-heading shelter__time-heading_balloon">Часы работы: </span>10:00 -\n' +
+                         '                18:00</p>',
+                     }
+                   }
                    options={
                      {
                        iconLayout: 'default#image',
@@ -180,6 +230,17 @@ const MapPage = () => {
                        iconImageOffset: [-20, -20],
                      }}/>
         <Placemark geometry={[55.684658, 37.738571]}
+                   properties={
+                     {
+                       balloonContentHeader: '              <h3 class="shelter__title shelter__title_balloon">\n' +
+                         '                Приют Бирюлево\n' +
+                         '              </h3>\n',
+                       balloonContentBody: '<p class="shelter__address shelter__address_balloon">Востряковский пр-д, 10А, Москва, Россия.</p>',
+                       balloonContentFooter: '              <p class="shelter__time shelter__time_balloon"><span\n' +
+                         '                class="shelter__time-heading shelter__time-heading_balloon">Часы работы: </span>10:00 -\n' +
+                         '                18:00</p>',
+                     }
+                   }
                    options={
                      {
                        iconLayout: 'default#image',
@@ -188,6 +249,17 @@ const MapPage = () => {
                        iconImageOffset: [-20, -20],
                      }}/>
         <Placemark geometry={[52.684758, 30.738521]}
+                   properties={
+                     {
+                       balloonContentHeader: '              <h3 class="shelter__title shelter__title_balloon">\n' +
+                         '                Приют Бирюлево\n' +
+                         '              </h3>\n',
+                       balloonContentBody: '<p class="shelter__address shelter__address_balloon">Востряковский пр-д, 10А, Москва, Россия.</p>',
+                       balloonContentFooter: '              <p class="shelter__time shelter__time_balloon"><span\n' +
+                         '                class="shelter__time-heading shelter__time-heading_balloon">Часы работы: </span>10:00 -\n' +
+                         '                18:00</p>',
+                     }
+                   }
                    options={
                      {
                        iconLayout: 'default#image',
@@ -196,6 +268,17 @@ const MapPage = () => {
                        iconImageOffset: [-20, -20],
                      }}/>
         <Placemark geometry={[51.684758, 39.738521]}
+                   properties={
+                     {
+                       balloonContentHeader: '              <h3 class="shelter__title shelter__title_balloon">\n' +
+                         '                Приют Бирюлево\n' +
+                         '              </h3>\n',
+                       balloonContentBody: '<p class="shelter__address shelter__address_balloon">Востряковский пр-д, 10А, Москва, Россия.</p>',
+                       balloonContentFooter: '              <p class="shelter__time shelter__time_balloon"><span\n' +
+                         '                class="shelter__time-heading shelter__time-heading_balloon">Часы работы: </span>10:00 -\n' +
+                         '                18:00</p>',
+                     }
+                   }
                    options={
                      {
                        iconLayout: 'default#image',
