5ea200a362059bb5cd66176178bb55e9358e331d
bevuxyna <wannhorn@gmail.com>
2023-01-15 23:15:30 +0300

    feat/user_form: сверстать компонент UserForm

---
+++ src/components/UserForm/UserForm.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './UserForm.css';
+
+function UserForm({ title, buttonText, onSubmit, children}) {
+    return (
+        <div className="user-form__container">
+            <h2 className="user-form__title">{title}</h2>
+
+            <form
+                className={`user-form`}
+                onSubmit={onSubmit}
+            >
+
+                {children}
+
+                <button
+                    className={`user-form__button-submit`}
+                    type="submit"
+                >
+                    {buttonText}
+                </button>
+
+            </form>
+        </div>
+    );
+}
+
+export default UserForm;
