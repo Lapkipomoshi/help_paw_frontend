3bab7193952618ba0f0d27c95060a849e5d039a3
Artyom <artyom-first@yandex.ru>
2023-01-17 21:42:52 +0500

    feat: прописать js-код for components Banner and SheltersOnMain

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -24,7 +24,7 @@ const App = (props) => {
       <Header
         loggedIn={loggedIn} />
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage loggedIn={loggedIn} />} />
         <Route path="/map" element={<MapPage />} />
         <Route path="/shelter-list" element={<ShelterListPage />} />
         <Route path="/shelters/:id" element={<ShelterPage />} />
--- src/components/PapersOnMain/PapersOnMain.jsx
+++ src/components/PapersOnMain/PapersOnMain.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import './PapersOnMain.css';
 import PaperCard from '../PaperCard/PaperCard';
 import paperPhoto1 from '../../images/paper-card__photo_1.jpg';
@@ -6,6 +7,8 @@ import paperPhoto2 from '../../images/paper-card__photo_2.jpg';
 import paperPhoto3 from '../../images/paper-card__photo_3.jpg';
 
 const PapersOnMain = () => {
+  const navigate = useNavigate(); 
+  
   return (
     <section className='papers-on-main'>
       <h2 className='section-title'>Полезные статьи</h2>
