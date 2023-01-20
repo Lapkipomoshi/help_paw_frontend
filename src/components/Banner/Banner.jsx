85bf2dbca18633561439114f57244a3153818408
Artyom <artyom-first@yandex.ru>
2023-01-21 00:48:26 +0500

    fix: заменить формат картинки in component Banner to png

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
 import './Banner.css';
-import bannerImage from '../../images/main__banner.svg';
+import bannerImage from '../../images/main__banner.png';
 
 const Banner = () => {
   const navigate = useNavigate(); // хук для использования программной навигации
