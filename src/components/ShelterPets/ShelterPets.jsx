ca24e1d16f73239c177465b2a9efacde6644d03b
Artyom <artyom-first@yandex.ru>
2023-01-21 20:06:10 +0500

    feat: подключить кнопки к слайдеру in component ShelterPets

--- src/components/ShelterPets/ShelterPets.jsx
+++ src/components/ShelterPets/ShelterPets.jsx
@@ -2,10 +2,65 @@ import React from 'react';
 import './ShelterPets.css';
 
 const ShelterPets = () => {
+  const slider = React.useRef(null);
+
   const [petsList, setPetsList] = React.useState([]); // список питомцев приюта
 
+  let position = 0;
+
+  const handlePrev = () => {
+    position -= 212;
+    slider.current.childNodes.forEach((element) => {
+      element.style = `transform: translateX(${position}px)`;
+    });
+  }
+
+  const handleNext = () => {
+    position += 212;
+    slider.current.childNodes.forEach((element) => {
+      element.style = `transform: translateX(${position}px)`;
+    });
+  }
+
   React.useEffect(() => {
-    setPetsList([1,2,3,4,5,6,7,8,9]);
+    setPetsList([ // будет запрашиваться с бэкенда
+      {id: 1,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+      {id: 2,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+      {id: 3,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+      {id: 4,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+      {id: 5,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+      {id: 6,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+      {id: 7,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+      {id: 8,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+      {id: 9,
+        name: 'Пушистик',
+        age: '2 года',
+        male: true},
+  ]);
   }, [])
 
   return (
@@ -18,14 +73,14 @@ const ShelterPets = () => {
               <h3 className='shelter-pets__summary-title'>Кошки</h3>
               <div className='shelter-pets__summary-icon' />
             </summary>
-            <ul className='slider'>
-              {petsList.map((card) => (
-                <li className='slider__item'></li>
+            <ul className='slider' ref={slider}>
+              {petsList.map((card, index) => (
+                <li className='slider__item' key={index}></li>
               ))}
             </ul>
             <div className='slider__buttons'>
-              <button className='slider__button slider__button' type='button' />
-              <button className='slider__button slider__button_next' type='button' />
+              <button className='slider__button slider__button' type='button' onClick={handlePrev} />
+              <button className='slider__button slider__button_next' type='button' onClick={handleNext} />
             </div>
           </details>
         </li>
