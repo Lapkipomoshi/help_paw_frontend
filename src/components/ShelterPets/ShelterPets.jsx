7686e113fbd0a0452cd2ebe53c5ed7830dc5b1aa
Artyom <artyom-first@yandex.ru>
2023-01-21 18:34:10 +0500

    feat: создать detalis-summary структуру и сверстать её для 1440px in component ShelterPets in SheterPage

--- src/components/ShelterPets/ShelterPets.jsx
+++ src/components/ShelterPets/ShelterPets.jsx
@@ -3,8 +3,28 @@ import './ShelterPets.css';
 
 const ShelterPets = () => {
   return (
-    <section className='shelter-section'>
+    <section className='shelter-section shelter-pets'>
       <h2 className='shelter-section__title'>Наши животные</h2>
+      <ul className='shelter-pets__list'>
+        <li className='shelter-pets__item'>
+          <details className='shelter-pets__detalis' open>
+            <summary className='shelter-pets__summary'>
+              <h3 className='shelter-pets__summary-title'>Кошки</h3>
+              <div className='shelter-pets__summary-icon' />
+            </summary>
+            <div className='shelter-pets__slider'>123</div>
+          </details>
+        </li>
+        <li className='shelter-pets__item'>
+          <details className='shelter-pets__detalis'>
+            <summary className='shelter-pets__summary'>
+              <h3 className='shelter-pets__summary-title'>Пёсики</h3>
+              <div className='shelter-pets__summary-icon' />
+            </summary>
+            <div className='shelter-pets__slider'>123</div>
+          </details>
+        </li>
+      </ul>
     </section>
   );
 }
