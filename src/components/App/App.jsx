cd42c86c656d803d005169264083841b16666e2a
Artyom <artyom-first@yandex.ru>
2023-01-24 21:36:57 +0500

    feat: добавить файл editorconfig

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,11 +8,11 @@ import MainPage from '../../pages/MainPage/MainPage';
 import MapPage from '../../pages/MapPage/MapPage';
 import ShelterListPage from '../../pages/ShelterListPage/ShelterListPage';
 import ShelterPage from '../../pages/ShelterPage/ShelterPage';
-  import AboutShelter from '../AboutShelter/AboutShelter';
-  import HelpToShelter from '../HelpToShelter/HelpToShelter';
-  import SheterNews from '../ShelterNews/ShelterNews';
-  import ShelterPets from '../ShelterPets/ShelterPets';
-  import ShelterVacancies from '../ShelterVacancies/ShelterVacancies';
+import AboutShelter from '../AboutShelter/AboutShelter';
+import HelpToShelter from '../HelpToShelter/HelpToShelter';
+import SheterNews from '../ShelterNews/ShelterNews';
+import ShelterPets from '../ShelterPets/ShelterPets';
+import ShelterVacancies from '../ShelterVacancies/ShelterVacancies';
 import PetPage from '../../pages/PetPage/PetPage';
 import PapersPage from '../../pages/PapersPage/PapersPage';
 import PaperPage from '../../pages/PaperPage/PaperPage';
@@ -26,33 +26,33 @@ const App = () => {
   const [loggedIn, setLoggedIn] = React.useState(false); // пользователь вошёл в учётную запись?
 
   return (
-    <div className="page">
+    <div className='page'>
       <Header
         loggedIn={loggedIn} />
       <Routes>
-        <Route path="/" element={<MainPage loggedIn={loggedIn} />} />
-        <Route path="/shelters" element={<MapPage />} />
-        <Route path="/shelters/list" element={<ShelterListPage />} />
-        <Route path="/shelters/:id" element={<ShelterPage />}>
+        <Route path='/' element={<MainPage loggedIn={loggedIn} />} />
+        <Route path='/shelters' element={<MapPage />} />
+        <Route path='/shelters/list' element={<ShelterListPage />} />
+        <Route path='/shelters/:id' element={<ShelterPage />}>
           <Route path='about' element={<AboutShelter />} />
           <Route path='how-to-help' element={<HelpToShelter />} />
           <Route path='news' element={<SheterNews />} />
           <Route path='pets' element={<ShelterPets />} />
           <Route path='vacancies' element={<ShelterVacancies />} />
         </Route>
-        <Route path="/pets/:id" element={<PetPage />} />
-        <Route path="/papers" element={<PapersPage />} />
-        <Route path="/papers/:id" element={<PaperPage />} />
-        <Route path="/news" element={<NewsPage />} />
-        <Route path="/news/:id" element={<NewPage />} />
-        <Route path="/sign-in" element={
+        <Route path='/pets/:id' element={<PetPage />} />
+        <Route path='/papers' element={<PapersPage />} />
+        <Route path='/papers/:id' element={<PaperPage />} />
+        <Route path='/news' element={<NewsPage />} />
+        <Route path='/news/:id' element={<NewPage />} />
+        <Route path='/sign-in' element={
           <ProtectedRoute loggedIn={!loggedIn} component={LoginPage} />
-          }/>
-        <Route path="/sign-up" element={
+        } />
+        <Route path='/sign-up' element={
           <ProtectedRoute loggedIn={!loggedIn} component={RegisterPage} />
-          }/>
-        <Route path="/help_paw_frontend" element={<Navigate to='/' replace />} />
-        <Route path="*" element={<NotFoundPage />} />
+        } />
+        <Route path='/help_paw_frontend' element={<Navigate to='/' replace />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
       <Footer />
     </div>
