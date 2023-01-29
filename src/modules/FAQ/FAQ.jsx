4d9a4eac3ff98a92bf52eac2a6d07546f18b521f
Artyom <artyom-first@yandex.ru>
2023-01-30 00:18:16 +0500

    refactor: переделать компонент FaqCard в компонент DetalisCard

--- src/modules/FAQ/FAQ.jsx
+++ src/modules/FAQ/FAQ.jsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import './Faq.css';
-import FaqCard from '../../components/FaqCard/FaqCard';
+import DetalisCard from '../../components/DetalisCard/DetalisCard';
 
 const Faq = () => {
   return (
     <section className='faq'>
       <h2 className='section-title'>Часто задаваемые вопросы</h2>
       <ul className='faq__list'>
-        <FaqCard title='Как пожертвовать деньги приюту?' isOpen>
+        <DetalisCard title='Как пожертвовать деньги приюту?' isOpen>
           <p className='faq__text'>
             Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”
           </p>
-        </FaqCard>
-        <FaqCard title='Как взять питомца домой?' isOpen={false}>
+        </DetalisCard>
+        <DetalisCard title='Как взять питомца домой?' isOpen={false}>
           <p className='faq__text'>
             Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”
           </p>
-        </FaqCard>
-        <FaqCard title='Как зарегистрировать приют?' isOpen={false}>
+        </DetalisCard>
+        <DetalisCard title='Как зарегистрировать приют?' isOpen={false}>
           <p className='faq__text'>
             Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”
           </p>
-        </FaqCard>
-        <FaqCard title='Какая минимальная сумма пожертвования?' isOpen={false}>
+        </DetalisCard>
+        <DetalisCard title='Какая минимальная сумма пожертвования?' isOpen={false}>
           <p className='faq__text'>
             Что бы сделать пожертвование, вам нужно открыть страницу приюта и нажать кнопку “Пожертвовать приюту”
           </p>
-        </FaqCard>
+        </DetalisCard>
       </ul>
     </section>
   );
