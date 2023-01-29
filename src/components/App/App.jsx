319e435ab26f8dbcd6ffcb80e30b5634f14d7fb8
Artyom <artyom-first@yandex.ru>
2023-01-29 17:17:33 +0500

    style: поправить в соответствии с linter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
 import ProtectedRoute from '../ProtectedRoute';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 import MainPage from '../../pages/MainPage/MainPage';
 import MapPage from '../../pages/MapPage/MapPage';
 import ShelterListPage from '../../pages/ShelterListPage/ShelterListPage';
+
 import ShelterPage from '../../pages/ShelterPage/ShelterPage';
 import AboutShelter from '../AboutShelter/AboutShelter';
 import HelpToShelter from '../HelpToShelter/HelpToShelter';
 import ShelterNews from '../ShelterNews/ShelterNews';
 import ShelterPets from '../ShelterPets/ShelterPets';
 import ShelterVacancies from '../ShelterVacancies/ShelterVacancies';
+
 import PetPage from '../../pages/PetPage/PetPage';
 import PapersPage from '../../pages/PapersPage/PapersPage';
 import PaperPage from '../../pages/PaperPage/PaperPage';
@@ -23,37 +25,46 @@ import RegisterPage from '../../pages/RegisterPage/RegisterPage';
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 
 const App = () => {
+  // eslint-disable-next-line no-unused-vars
   const [loggedIn, setLoggedIn] = React.useState(false); // пользователь вошёл в учётную запись?
 
   return (
-    <div className="page">
+    <div className='page'>
       <Header
-        loggedIn={loggedIn}/>
+        loggedIn={loggedIn}
+      />
       <Routes>
-        <Route path="/" element={<MainPage loggedIn={loggedIn}/>}/>
-        <Route path="/shelters" element={<MapPage/>}/>
-        <Route path="/shelters/list" element={<ShelterListPage/>}/>
-        <Route path="/shelters/:id" element={<ShelterPage/>}>
-          <Route path="about" element={<AboutShelter/>}/>
-          <Route path="how-to-help" element={<HelpToShelter/>}/>
-          <Route path="news" element={<ShelterNews/>}/>
-          <Route path="pets" element={<ShelterPets/>}/>
-          <Route path="vacancies" element={<ShelterVacancies/>}/>
+        <Route path='/' element={<MainPage loggedIn={loggedIn} />} />
+        <Route path='/shelters' element={<MapPage />} />
+        <Route path='/shelters/list' element={<ShelterListPage />} />
+        <Route path='/shelters/:id' element={<ShelterPage />}>
+          <Route path='about' element={<AboutShelter />} />
+          <Route path='how-to-help' element={<HelpToShelter />} />
+          <Route path='news' element={<ShelterNews />} />
+          <Route path='pets' element={<ShelterPets />} />
+          <Route path='vacancies' element={<ShelterVacancies />} />
         </Route>
-        <Route path="/pets/:id" element={<PetPage />} />
-        <Route path="/papers" element={<PapersPage/>}/>
-        <Route path="/papers/:id" element={<PaperPage/>}/>
-        <Route path="/news" element={<NewsPage/>}/>
-        <Route path="/news/:id" element={<NewPage/>}/>
-        <Route path="/sign-in" element={
-          <ProtectedRoute loggedIn={!loggedIn} component={LoginPage}/>
-        }/>
-        <Route path="/sign-up" element={
-          <ProtectedRoute loggedIn={!loggedIn} component={RegisterPage}/>
-        }/>
-        <Route path="*" element={<NotFoundPage/>}/>
+        <Route path='/pets/:id' element={<PetPage />} />
+        <Route path='/papers' element={<PapersPage />} />
+        <Route path='/papers/:id' element={<PaperPage />} />
+        <Route path='/news' element={<NewsPage />} />
+        <Route path='/news/:id' element={<NewPage />} />
+        <Route
+          path='/sign-in'
+          element={
+            <ProtectedRoute loggedIn={!loggedIn} component={LoginPage} />
+          }
+        />
+        <Route
+          path='/sign-up'
+          element={
+            <ProtectedRoute loggedIn={!loggedIn} component={RegisterPage} />
+          }
+        />
+        <Route path='/help_paw_frontend' element={<Navigate to='/' replace />} />
+        <Route path='*' element={<NotFoundPage />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
