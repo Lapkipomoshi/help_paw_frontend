97b1abdf954860bd3e0241e7d6545fd687a55f13
Artyom <artyom-first@yandex.ru>
2023-01-30 01:18:04 +0500

    refactor: render VacancyCards in molule ShelterVacancies by array.map

--- src/modules/ShelterVacancies/ShelterVacancies.jsx
+++ src/modules/ShelterVacancies/ShelterVacancies.jsx
@@ -3,28 +3,49 @@ import './ShelterVacancies.css';
 import VacancyCard from '../../components/VacancyCard/VacancyCard';
 
 const ShelterVacancies = () => {
+  const [vacanciesList, setVacanciesList] = React.useState([]); // список вакансий
+
+  React.useEffect(() => {
+    setVacanciesList([ // будет запрашиваться с бэкенда
+      {
+        id: 1,
+        title: 'Помощник уборщика',
+        salary: '35 000 рублей до НДФЛ',
+        workSchedule: 'сменный',
+        charge: 'помогать уборщику убирать мусор',
+      },
+      {
+        id: 2,
+        title: 'Помощник уборщика',
+        salary: '35 000 рублей до НДФЛ',
+        workSchedule: 'сменный',
+        charge: 'помогать уборщику убирать мусор',
+      },
+      {
+        id: 3,
+        title: 'Помощник уборщика',
+        salary: '35 000 рублей до НДФЛ',
+        workSchedule: 'сменный',
+        charge: 'помогать уборщику убирать мусор',
+      },
+    ]);
+  }, []);
+
   return (
     <section className='shelter-section shelter-vacancies'>
       <h2 className='shelter-section__title'>Наши вакансии</h2>
       <ul className='vacancies-list'>
-        <VacancyCard
-          title='Помощник уборщика'
-          salary='35 000 рублей до НДФЛ'
-          workSchedule='сменный'
-          charge='помогать уборщику убирать мусор'
-        />
-        <VacancyCard
-          title='Помощник уборщика'
-          salary='35 000 рублей до НДФЛ'
-          workSchedule='сменный'
-          charge='помогать уборщику убирать мусор'
-        />
-        <VacancyCard
-          title='Помощник уборщика'
-          salary='35 000 рублей до НДФЛ'
-          workSchedule='сменный'
-          charge='помогать уборщику убирать мусор'
-        />
+        {vacanciesList.map((card) => {
+          return (
+            <VacancyCard
+              id={card.id}
+              title={card.title}
+              salary={card.salary}
+              workSchedule={card.workSchedule}
+              charge={card.charge}
+            />
+          );
+        })}
       </ul>
     </section>
   );
