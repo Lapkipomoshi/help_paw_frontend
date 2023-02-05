6cfe95a69c3051284a0324943017e7f0d0cbfd74
Artyom <artyom-first@yandex.ru>
2023-02-05 18:42:38 +0500

    refactor: вынести svg в отдельный файл in DetailsCard component

--- src/components/DetailsCard/DetailsCard.jsx
+++ src/components/DetailsCard/DetailsCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './DetailsCard.css';
+import PlusMinus from './svg/PlusMinus';
 
 const DetailsCard = ({ children, title, isOpen }) => {
   return (
@@ -7,10 +8,7 @@ const DetailsCard = ({ children, title, isOpen }) => {
       <details className='details-card__details' open={isOpen}>
         <summary className='details-card__row'>
           <h3 className='details-card__title'>{title}</h3>
-          <svg className='details-card__icon' width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
-            <line id='svg-vertical' x1='16.5' y1='1.5' x2='16.5' y2='30.5' stroke='#111c2c' strokeWidth='3' strokeLinecap='round' />
-            <line x1='30.5' y1='16.5' x2='1.5' y2='16.5' stroke='#111c2c' strokeWidth='3' strokeLinecap='round' />
-          </svg>
+          <PlusMinus />
         </summary>
         {children}
       </details>
