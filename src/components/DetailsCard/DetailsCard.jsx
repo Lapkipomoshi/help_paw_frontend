043e2e2ddff99e0b41998e64c94f7c86b2480658
Artyom <artyom-first@yandex.ru>
2023-02-05 22:15:57 +0500

    refactor: переписать стили в DetailsCard

--- src/components/DetailsCard/DetailsCard.jsx
+++ src/components/DetailsCard/DetailsCard.jsx
@@ -3,13 +3,13 @@ import './DetailsCard.css';
 import PlusMinus from './svg/PlusMinus';
 
 const DetailsCard = ({
-  children, title, isOpen, iconType,
+  children, title, isOpen, textStyle, iconType,
 }) => {
   return (
     <li className='details-card'>
       <details className='details-card__details' open={isOpen}>
         <summary className='details-card__row'>
-          <h3 className='details-card__title'>{title}</h3>
+          <p className={`details-card__title details-card__title_style_${textStyle}`}>{title}</p>
           {iconType === 'plus-minus' && <PlusMinus />}
         </summary>
         {children}
--- src/components/DetailsCard/svg/PlusMinus.jsx
+++ src/components/DetailsCard/svg/PlusMinus.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './PlusMinus.css';
 
 const PlusMinus = () => {
   return (
-    <svg className='details-card__icon_type_plus-minus' width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <line id='svg-vertical' x1='16.5' y1='1.5' x2='16.5' y2='30.5' stroke='#111c2c' strokeWidth='3' strokeLinecap='round' />
-      <line x1='30.5' y1='16.5' x2='1.5' y2='16.5' stroke='#111c2c' strokeWidth='3' strokeLinecap='round' />
+    <svg className='plus-minus' width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
+      <line className='plus-minus__vertical' x1='16.5' y1='1.5' x2='16.5' y2='30.5' strokeWidth='3' strokeLinecap='round' />
+      <line x1='30.5' y1='16.5' x2='1.5' y2='16.5' strokeWidth='3' strokeLinecap='round' />
     </svg>
   );
 };
