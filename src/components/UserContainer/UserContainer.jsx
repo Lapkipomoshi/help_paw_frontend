68e49005409432281a4c80d8c29185b00c6844fc
bevuxyna <wannhorn@gmail.com>
2023-02-23 17:46:04 +0300

    fix: заменить картинку собаки с шариками на отдельные элементы

--- src/components/UserContainer/UserContainer.jsx
+++ src/components/UserContainer/UserContainer.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './UserContainer.css';
 import Dog from '../../images/dog.png';
+import VioletBall from '../Ball/VioletBall/VioletBall';
+import OrangeBall from '../Ball/OrangeBall/OrangeBall';
+import TurquoiseBall from '../Ball/TurquoiseBall/TurquoiseBall';
 
 function UserContainer({ containerClass, children }) {
   return (
     <div className={`user-container user-container_${containerClass}`}>
       <img className='user-container__image' src={Dog} alt='Собака' />
+      <VioletBall modifier='user' />
+      <OrangeBall modifier='user' />
+      <TurquoiseBall modifier='user' />
 
       {children}
 
