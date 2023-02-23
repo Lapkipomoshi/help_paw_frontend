a2daaa755964a13ccba9b5a0e1fce38734f163d1
bevuxyna <wannhorn@gmail.com>
2023-02-23 17:46:46 +0300

    feat: сверстать страницу профиля пользователя

---
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ProfilePage.css';
+import ProfileContainer from '../../components/ProfileContainer/ProfileContainer';
+import ShelterCard from '../../components/ShelterCard/ShelterCard';
+
+const ProfilePage = () => {
+  return (
+    <main className='main'>
+      <section className='profile'>
+        <ProfileContainer>
+          <h2 className='profile__title'>Здравствуйте, Александр!</h2>
+          <p className='profile__subtitle'>Приюты, которым Вы помогли</p>
+
+          <div className='profile__shelters'>
+            <ShelterCard />
+            <ShelterCard />
+            <ShelterCard />
+          </div>
+        </ProfileContainer>
+      </section>
+    </main>
+  );
+};
+
+export default ProfilePage;
