5bc24f2667aeee9b698a0d7426c949436bbac61d
Artyom <artyom-first@yandex.ru>
2023-03-14 22:56:41 +0500

    feat: сверстать шаг 2 у формы на странице AddShelterPage

--- src/pages/AddShelterPage/AddShelterPage.jsx
+++ src/pages/AddShelterPage/AddShelterPage.jsx
@@ -35,11 +35,11 @@ const AddShelterPage = ({ currentUser }) => {
           {step === 1 && (
             <>
               <p className='add-shelter-form__caption'>ФИО владельца приюта*</p>
-              <input className='add-shelter-form__input' type='text' name='owner' />
+              <input className='add-shelter-form__input' type='text' name='owner' required />
               <p className='add-shelter-form__caption'>Номер телефона*</p>
-              <input className='add-shelter-form__input' type='tel' name='phone' />
+              <input className='add-shelter-form__input' type='tel' name='phone' required />
               <p className='add-shelter-form__caption'>E-mail*</p>
-              <input className='add-shelter-form__input' type='email' name='email' />
+              <input className='add-shelter-form__input' type='email' name='email' required />
               <div className='add-shelter-form__buttons'>
                 <Button submit>Далее</Button>
                 <Button theme='transparent'>Отменить</Button>
@@ -49,19 +49,26 @@ const AddShelterPage = ({ currentUser }) => {
           {step === 2 && (
             <>
               <p className='add-shelter-form__caption'>Логотип приюта</p>
-              <input className='add-shelter-form__logo' type='file' name='logo' />
+              <div className='personal-image'>
+                <label>
+                  <input type='file' name='logo' />
+                  <figure className='add-shelter-form__logo'>
+                    <div className='personal-avatar' />
+                  </figure>
+                </label>
+              </div>
               <p className='add-shelter-form__caption'>Часы приюта*</p>
               <div className='add-shelter-form__clock'>
-                <input className='add-shelter-form__time-input' type='time' name='working_from_hour' />
-                <p className='add-shelter-form__dash'>-</p>
-                <input className='add-shelter-form__time-input' type='time' name='working_from_hour' />
+                <input className='add-shelter-form__time-input' type='text' name='working_from_hour' placeholder='00:00' required />
+                <p>-</p>
+                <input className='add-shelter-form__time-input' type='text' name='working_from_hour' placeholder='00:00' required />
               </div>
               <div className='add-shelter-form__flex'>
                 <div className='add-shelter-form__column'>
                   <p className='add-shelter-form__caption'>Название приюта*</p>
-                  <input className='add-shelter-form__input' type='text' name='name' />
+                  <input className='add-shelter-form__input' type='text' name='name' required />
                   <p className='add-shelter-form__caption'>ИНН*</p>
-                  <input className='add-shelter-form__input' type='number' name='INN' />
+                  <input className='add-shelter-form__input' type='number' name='INN' required />
                   <p className='add-shelter-form__caption'>Ссылка на сайт приюта</p>
                   <input className='add-shelter-form__input' type='url' name='web_site' />
                   <p className='add-shelter-form__caption'>Ссылка на канал приюта в &laquo;Telegram&raquo;</p>
@@ -69,9 +76,9 @@ const AddShelterPage = ({ currentUser }) => {
                 </div>
                 <div className='add-shelter-form__column'>
                   <p className='add-shelter-form__caption'>Виды животных*</p>
-                  <input className='add-shelter-form__input' type='text' name='animal' />
+                  <input className='add-shelter-form__input' type='text' name='animal' required />
                   <p className='add-shelter-form__caption'>Адрес приюта*</p>
-                  <input className='add-shelter-form__input' type='text' name='address' />
+                  <input className='add-shelter-form__input' type='text' name='address' required />
                   <p className='add-shelter-form__caption'>Ссылка на группу приюта в &laquo;Одноклассники&raquo;</p>
                   <input className='add-shelter-form__input' type='url' name='ok_page' />
                   <p className='add-shelter-form__caption'>Ссылка на группу приюта в &laquo;VK&raquo;</p>
@@ -79,7 +86,7 @@ const AddShelterPage = ({ currentUser }) => {
                 </div>
               </div>
               <p className='add-shelter-form__caption'>Описание приюта*</p>
-              <textarea className='add-shelter-form__textarea' name='description' />
+              <textarea className='add-shelter-form__textarea' name='description' required />
               <div className='register__privacy'>
                 <label className='checkbox__container'>
                   <input type='checkbox' className='checkbox__input' onClick={handleChangeCheckbox} />
