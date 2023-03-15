37675caa686d084e8cd1985956720037ef941c6b
bevuxyna <wannhorn@gmail.com>
2023-03-15 22:48:25 +0300

    fix: исправить попап личного кабинета

--- src/components/ProfilePopup/ProfilePopup.jsx
+++ src/components/ProfilePopup/ProfilePopup.jsx
@@ -1,20 +1,19 @@
 import React, { useContext } from 'react';
 import './ProfilePopup.css';
 import { Link } from 'react-router-dom';
-import Button from '../../ui/Button/Button';
 import CurrentUserContext from '../../contexts/CurrentUserContext';
 
-function ProfilePopup({ isOpen, closeProfilePopup, onSignout }) {
+function ProfilePopup({ isOpen, closeProfilePopup }) {
   const currentUser = useContext(CurrentUserContext);
   const { username, email } = currentUser;
 
   return (
     <div className={`profile-popup ${isOpen && 'profile-popup_opened'}`}>
-      <p className='profile-popup__name'>{username}</p>
+      <Link className='profile-popup__name' to='/profile' onClick={closeProfilePopup}>{username}</Link>
       <p className='profile-popup__email'>{email}</p>
-      <Link className='profile-popup__link' to='/profile/edit'>Редактировать</Link>
-      <Link className='profile-popup__link' to='/profile/edit-password'>Изменить пароль</Link>
-      <Button className='profile-popup__button' submit onClick={onSignout}>Выйти</Button>
+      <Link className='profile-popup__link' to='/profile/edit' onClick={closeProfilePopup}>Редактировать</Link>
+      <Link className='profile-popup__link' to='/profile/edit-password' onClick={closeProfilePopup}>Изменить пароль</Link>
+      <Link className='profile-popup__button' to='/profile/sign-out' onClick={closeProfilePopup}>Выйти</Link>
       <button
         type='button'
         className='profile-popup__button-close'
