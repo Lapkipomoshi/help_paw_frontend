e581d54aff7e3ec83c71c391dc3b19486e5f90dc
Artyom <artyom-first@yandex.ru>
2023-04-18 22:32:14 +0500

    feat(AddShelterPage): stylize the CheckboxesSelect ui-comoinent

--- src/ui/CheckboxesSelect/CheckboxesSelect.jsx
+++ src/ui/CheckboxesSelect/CheckboxesSelect.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-case-declarations */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React, { useEffect, useState } from 'react';
@@ -35,27 +36,61 @@ const CheckboxesSelect = ({ caption }) => {
     }
   };
 
+  const handleDeleteType = (e) => {
+    switch (e.target.dataset.type) {
+    case catType.caption:
+      const catCheck = document.querySelector(`#${catType.name}`);
+      catCheck.checked = !catCheck.checked;
+      setCat(false);
+      break;
+    case dogType.caption:
+      const dogCheck = document.querySelector(`#${dogType.name}`);
+      dogCheck.checked = !dogCheck.checked;
+      setDog(false);
+      break;
+    case parrotType.caption:
+      const parrotCheck = document.querySelector(`#${parrotType.name}`);
+      parrotCheck.checked = !parrotCheck.checked;
+      setParrot(false);
+      break;
+    case hamsterType.caption:
+      const hamsterCheck = document.querySelector(`#${hamsterType.name}`);
+      hamsterCheck.checked = !hamsterCheck.checked;
+      setHamster(false);
+      break;
+    default:
+      break;
+    }
+  };
+
   useEffect(() => {
-    setItems([isCat && catType.caption, isDog && dogType.caption, isParrot && parrotType.caption, isHamster && hamsterType.caption]);
+    const newSet = [];
+    if (isCat) { newSet.push(catType.caption); }
+    if (isDog) { newSet.push(dogType.caption); }
+    if (isParrot) { newSet.push(parrotType.caption); }
+    if (isHamster) { newSet.push(hamsterType.caption); }
+    setItems([...newSet]);
   }, [isCat, isDog, isParrot, isHamster]);
 
   return (
     <li className='checkboxes-select'>
       <label className='checkboxes-select__caption'>{caption}</label>
       <div className='checkboxes-select__multiselect'>
-        <div className='checkboxes-select__selectBox' onClick={() => { setOpenedSelect(!openedSelect); }}>
+        <div className='checkboxes-select__select-box'>
           <ul className='checkboxes-select__items'>
             {items && items.map((item, index) => {
               return (
                 // eslint-disable-next-line react/no-array-index-key
                 <li className='checkboxes-select__item' key={index}>
                   <p className='checkboxes-select__item-text'>{item}</p>
-                  <button className='checkboxes-select__delete' type='button' />
+                  <button className='checkboxes-select__delete' type='button' data-type={item} onClick={(e) => { handleDeleteType(e); }} />
                 </li>
               );
             })}
           </ul>
-          <Arrow openedSelect={openedSelect} />
+          <button className='checkboxes-select__arrow' type='button' onClick={() => { setOpenedSelect(!openedSelect); }}>
+            <Arrow openedSelect={openedSelect} />
+          </button>
         </div>
         <div className={`checkboxes-select__checkboxes ${openedSelect && 'checkboxes-select__checkboxes_opened'}`}>
           <label className='checkboxes-select__check-label' htmlFor={catType.name}>
