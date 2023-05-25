2835bf3ee2ec9d4032d664f858c4bfbd03b9b5e4
TatianaP <tanya.email4work@gmail.com>
2023-05-25 21:23:23 +0400

    feat: add pagination for article request

--- src/pages/PapersPage/PapersPage.jsx
+++ src/pages/PapersPage/PapersPage.jsx
@@ -5,18 +5,33 @@ import PaperCard from '../../components/PaperCard/PaperCard';
 import Button from '../../ui/Button/Button';
 import papersApi from './api';
 
+const PAPERS_PER_PAGE = 1;
+
 const PapersPage = () => {
   const [papersList, setPapersList] = useState([]); // список отображаемых карточек со статьями
+  const [papersPage, setPapersPage] = useState(0); // текущая загруженная страница статей
+  const [hasMorePapers, setHasMorePapers] = useState(false);
 
-  useEffect(() => {
+  const fetchPapers = () => {
     papersApi
-      .getPapers(9) // загрузка статей
+      .getPapers(PAPERS_PER_PAGE, papersPage * PAPERS_PER_PAGE) // загрузка статей
       .then((res) => {
-        setPapersList(res.results);
+        setPapersList((prev) => { return [...prev, ...res.results]; });
+        setPapersPage(papersPage + 1);
+
+        setHasMorePapers(Boolean(res.next));
       })
       .catch((err) => {
         throw new Error(err);
       });
+  };
+
+  const handleClick = () => {
+    fetchPapers();
+  };
+
+  useEffect(() => {
+    fetchPapers();
   }, []);
 
   return (
@@ -24,11 +39,13 @@ const PapersPage = () => {
       <main className='main papers'>
         <div className='papers__head-block'>
           <h1 className='papers__title'>Полезные статьи</h1>
-          <Button className='margin-left_auto' to='/shelters' link>Хочу помогать</Button>
+          <Button className='margin-left_auto button--mobile-hidden' to='/shelters' link>
+            Хочу помогать
+          </Button>
         </div>
         <ul className='papers__grid'>
-          {papersList && papersList.length !== 0
-            ? papersList.map((card) => {
+          {papersList && papersList.length !== 0 ? (
+            papersList.map((card) => {
               return (
                 <li className='papers__grid-element' key={card.id}>
                   <PaperCard
@@ -39,9 +56,17 @@ const PapersPage = () => {
                 </li>
               );
             })
-            : <p>Не удалось загрузить статьи</p>}
+          ) : (
+            <p>Не удалось загрузить статьи</p>
+          )}
         </ul>
-        <Button className='margin_column-center'>Больше статей</Button>
+        <Button
+          className='button--full-width'
+          disabled={!hasMorePapers}
+          onClick={handleClick}
+        >
+          Больше статей
+        </Button>
       </main>
     </MainContainer>
   );
--- src/pages/PapersPage/api.js
+++ src/pages/PapersPage/api.js
@@ -7,13 +7,13 @@ class PapersApi extends BaseApi {
     this._headers = _headers;
   }
 
-  getPapers(amount) { // загрузка указанного количества статей
-    return fetch(`${this._baseUrl}/v1/help-articles/?limit=${amount}`, {
+  getPapers(amount, offset = 0) {
+    // загрузка указанного количества статей
+    return fetch(`${this._baseUrl}/v1/help-articles/?limit=${amount}&offset=${offset}`, {
       headers: this._headers,
-    })
-      .then((res) => {
-        return super._processTheResponse(res);
-      });
+    }).then((res) => {
+      return super._processTheResponse(res);
+    });
   }
 }
 
