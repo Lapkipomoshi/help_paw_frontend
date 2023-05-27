d80cfd9b52636ef1d7bf8dcabda1d63e7b7eef1f
TatianaP <tanya.email4work@gmail.com>
2023-05-27 18:09:24 +0400

    'fix: PR comments'

--- src/pages/PapersPage/PapersPage.jsx
+++ src/pages/PapersPage/PapersPage.jsx
@@ -1,24 +1,46 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import './PapersPage.scss';
 import MainContainer from '../../components/MainContainer/MainContainer';
 import PaperCard from '../../components/PaperCard/PaperCard';
 import Button from '../../ui/Button/Button';
 import papersApi from './api';
 
-const PAPERS_PER_PAGE = 1;
+const PapersAmountByBreakpoint = { mobile: 2, tablet: 4, desktop: 8 };
 
 const PapersPage = () => {
   const [papersList, setPapersList] = useState([]); // список отображаемых карточек со статьями
-  const [papersPage, setPapersPage] = useState(0); // текущая загруженная страница статей
   const [hasMorePapers, setHasMorePapers] = useState(false);
+
+  const isMobile = useMediaQuery({
+    maxWidth: 767,
+  });
+
+  const isTablet = useMediaQuery({
+    maxWidth: 1439,
+    minWidth: 768,
+  });
+
+  const isDesktop = useMediaQuery({
+    minWidth: 1440,
+  });
+
+  const papersAmount = useMemo(() => {
+    switch (true) {
+    case isMobile:
+      return PapersAmountByBreakpoint.mobile;
+    case isTablet:
+      return PapersAmountByBreakpoint.tablet;
+    default:
+      return PapersAmountByBreakpoint.desktop;
+    }
+  }, [isMobile, isTablet, isDesktop]);
 
   const fetchPapers = () => {
     papersApi
-      .getPapers(PAPERS_PER_PAGE, papersPage * PAPERS_PER_PAGE) // загрузка статей
+      .getPapers(papersAmount, papersList.length)
       .then((res) => {
-        setPapersList((prev) => { return [...prev, ...res.results]; });
-        setPapersPage(papersPage + 1);
-
+        setPapersList([...papersList, ...res.results]);
         setHasMorePapers(Boolean(res.next));
       })
       .catch((err) => {
@@ -38,8 +60,8 @@ const PapersPage = () => {
     <MainContainer>
       <main className='main papers'>
         <div className='papers__head-block'>
-          <h1 className='papers__title'>Полезные статьи</h1>
-          <Button className='margin-left_auto button--mobile-hidden' to='/shelters' link>
+          <h1 className='papers__title standart-font_type_h2'>Полезные статьи</h1>
+          <Button className='margin-left_auto papers__offer-button' to='/shelters' link>
             Хочу помогать
           </Button>
         </div>
@@ -60,13 +82,14 @@ const PapersPage = () => {
             <p>Не удалось загрузить статьи</p>
           )}
         </ul>
-        <Button
-          className='button--full-width'
-          disabled={!hasMorePapers}
-          onClick={handleClick}
-        >
-          Больше статей
-        </Button>
+        {hasMorePapers && (
+          <Button
+            className='papers__load-more-button'
+            onClick={handleClick}
+          >
+            Больше статей
+          </Button>
+        )}
       </main>
     </MainContainer>
   );
--- src/pages/PapersPage/api.js
+++ src/pages/PapersPage/api.js
@@ -7,8 +7,7 @@ class PapersApi extends BaseApi {
     this._headers = _headers;
   }
 
-  getPapers(amount, offset = 0) {
-    // загрузка указанного количества статей
+  getPapers(amount, offset = 0) { // загрузка указанного количества статей
     return fetch(`${this._baseUrl}/v1/help-articles/?limit=${amount}&offset=${offset}`, {
       headers: this._headers,
     }).then((res) => {
