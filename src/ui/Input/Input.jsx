12b62f3abffecf228f692f1be58e11d15e6f30af
Vlada Melnikova <wannhorn@gmail.com>
2023-05-31 22:08:48 +0300

    feat: добавить адаптив для Input

--- src/ui/Input/Input.jsx
+++ src/ui/Input/Input.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Input.css';
+import './Input.scss';
 
 function Input({
   labelText, inputName, inputType, spanText, onChange, minLength, maxLength, errorMessage, isValid, value,
 }) {
   return (
     <div className='input__wrapper'>
-      <label className='input__label'>{labelText}</label>
+      <label className='input__label standart-font standart-font_type_body'>{labelText}</label>
       <input
-        className={`input ${errorMessage && 'input_invalid'} ${isValid && 'input_valid'}`}
+        className={`input standart-font standart-font_type_body ${errorMessage && 'input_invalid'} ${isValid && 'input_valid'}`}
         name={inputName}
         type={inputType}
         minLength={minLength}
@@ -17,7 +17,7 @@ function Input({
         required
         onChange={onChange}
       />
-      <span className='input__error'>{spanText}</span>
+      <span className='input__error standart-font standart-font_type_smallest'>{spanText}</span>
     </div>
   );
 }
