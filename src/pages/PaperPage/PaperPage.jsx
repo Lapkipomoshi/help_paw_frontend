2b9626d92046e8474ea09f27fe599cd2374eb35c
TatianaP <tanya.email4work@gmail.com>
2023-06-08 22:51:57 +0400

    feat: add error page displaying when the PaperPage does not exist

--- src/pages/PaperPage/PaperPage.jsx
+++ src/pages/PaperPage/PaperPage.jsx
@@ -2,22 +2,37 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './PaperPage.scss';
 import MainContainer from '../../components/MainContainer/MainContainer';
+import NotFoundPage from '../NotFoundPage/NotFoundPage';
 import paperApi from './api';
 
 const PaperPage = () => {
   const { id } = useParams(); // id статьи, получаемый из url-адреса текущей страницы
-  const [paper, setPaper] = useState({}); // информация о статье
+  const [paper, setPaper] = useState(null); // информация о статье
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     paperApi
       .getPaperById(id) // загрузка статьи с указанным id
       .then((res) => {
         setPaper(res);
+        setIsLoading(false);
       })
       .catch((err) => {
+        setPaper(null);
+        setIsLoading(false);
         throw new Error(err);
       });
   }, [id]);
+
+  if (isLoading) {
+    return null;
+  }
+
+  const isDataNotFound = !paper && !isLoading;
+
+  if (isDataNotFound) {
+    return <NotFoundPage />;
+  }
 
   return (
     <MainContainer>
