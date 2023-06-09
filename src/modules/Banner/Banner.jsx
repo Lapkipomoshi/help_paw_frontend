7f37d317fa4c283b93195054e939c1b54245c0d8
Artyom <artyom-first@yandex.ru>
2023-06-09 23:02:02 +0500

    feat(Banner): add adaptiv and rewrite to SCSS
    https://lapkipomoshi.atlassian.net/browse/UMAK-163
    
    Не придумал ничего иного, кроме как сделать 2 заголовка в секции, так как порядок меняется порядок элементов на мобильной версии

--- src/modules/Banner/Banner.jsx
+++ src/modules/Banner/Banner.jsx
@@ -7,7 +7,7 @@ import bannerImage from '../../images/main__banner.png';
 const Banner = () => {
   return (
     <MainContainer>
-      <section className='banner section'>
+      <section className='banner main-page-section'>
         <h1 className='banner__title standard-font standard-font_type_h1'>Помогаем тем, кто в этом нуждается</h1>
         <div className='banner__line' />
         <p className='banner__subtitle standard-font standard-font_type_h3'>Лапки помощи - это проект помощи приютам для животных.</p>
