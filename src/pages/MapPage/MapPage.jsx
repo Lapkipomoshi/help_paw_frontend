ae877728163ce177f2e7d3f4d94ec7360d742983
TatianaP <tanya.email4work@gmail.com>
2023-06-13 18:58:59 +0400

    fix: remove unused lines

--- src/pages/MapPage/MapPage.jsx
+++ src/pages/MapPage/MapPage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import { useInView } from 'react-intersection-observer';
 import { Link } from 'react-router-dom';
 import './MapPage.css';
 import { Map, Placemark, YMaps } from '@pbe/react-yandex-maps';
