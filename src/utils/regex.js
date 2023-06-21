87819a9fb88bdab4e6682d007ae9bf284a7a3c88
Vlada Melnikova <wannhorn@gmail.com>
2023-06-21 22:22:16 +0300

    UMAK-203: fix regex

--- src/utils/regex.js
+++ src/utils/regex.js
@@ -3,7 +3,7 @@ export const NAME_REGEX = /^[A-Za-zа-яА-ЯёЁ\-_\s]+$/;
 export const EMAIL_REGEX = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i;
 export const PASSWORD_REGEX = /^[a-zA-Z0-9[\]!@#$%^&amp;*()_+=}{<>:'"?/\\|.,~-]+$/;
 export const NUMBER = /^\d+$/;
-export const SYMBOL = /^\W+$/;
+export const SYMBOL = /^[-_]+$/;
 export const URL = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 export const TEL = /^([+]?[0-9\s-()]{10,20})+$/i;
 export const TIME = /^([01][0-9]|2[0-3]):[0-5][0-9]+$/;
