7d62fe4b596d5b83ccc7f913d9ada9b11267be01
Vlada Melnikova <wannhorn@gmail.com>
2023-06-22 16:01:51 +0300

    UMAK-194: Закрытие popup по клику вне области popup https://lapkipomoshi.atlassian.net/jira/software/projects/UMAK/boards/1/backlog?selectedIssue=UMAK-194

--- src/components/InfoTooltip/InfoTooltip.jsx
+++ src/components/InfoTooltip/InfoTooltip.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './InfoTooltip.scss';
 
 function InfoTooltip({
-  isOpen, image, message,
+  isOpen, image, message, onClose,
 }) {
+  const popup = useRef(null);
+  function handleOverlayClose(evt) {
+    if (evt.target.classList.contains('popup_opened') || evt.target.classList.contains('popup__button-close')) {
+      onClose();
+    }
+  }
+
+  function handleEscClose(evt) {
+    if (evt.key === 'Escape') {
+      onClose();
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleEscClose);
+    document.addEventListener('click', handleOverlayClose);
+    return () => {
+      document.removeEventListener('keydown', handleEscClose);
+      document.removeEventListener('click', handleOverlayClose);
+    };
+  }, []);
+
   return (
     <div
       className={`popup ${isOpen && 'popup_opened'}`}
+      ref={popup}
     >
       <div className='popup__container'>
         <img
@@ -20,7 +43,7 @@ function InfoTooltip({
         <button
           type='button'
           className='popup__button-close'
-          // onClick={onClose}
+          onClick={onClose}
         />
       </div>
     </div>
