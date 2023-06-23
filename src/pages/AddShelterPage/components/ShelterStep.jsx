abe01f3a497c8324bee23fedc6f7993e5ef185d3
Artyom <artyom-first@yandex.ru>
2023-06-23 21:30:12 +0500

    feat(AddShelterPage): add the mainPhoto inpit

--- src/pages/AddShelterPage/components/ShelterStep.jsx
+++ src/pages/AddShelterPage/components/ShelterStep.jsx
@@ -11,12 +11,20 @@ import * as errorMessage from '../../../utils/errorMessage';
 // шаг в форме добавления приюта с анкетой о самом приюте
 const ShelterStep = ({ handleBack, setShelter }) => {
   const [logo, setLogo] = useState();
+  const [mainPhoto, setMainPhoto] = useState();
   const startTime = useInput('', { notEmpty: true, regex: regex.TIME }, errorMessage.TIME);
   const finishTime = useInput('', { notEmpty: true, regex: regex.TIME }, errorMessage.TIME);
   const shelterName = useInput('', { notEmpty: true, maxLength: 50, regex: regex.NAME_REGEX }, errorMessage.SHELTER_NAME);
-  const INN = useInput('', {
-    notEmpty: true, minLength: 10, maxLength: 10, regex: regex.NUMBER,
-  }, errorMessage.INN);
+  const INN = useInput(
+    '',
+    {
+      notEmpty: true,
+      minLength: 10,
+      maxLength: 10,
+      regex: regex.NUMBER,
+    },
+    errorMessage.INN
+  );
   const webSite = useInput('', { regex: regex.URL, maxLength: 200 }, errorMessage.SHELTER_SITE);
   const telegram = useInput('', { regex: regex.TELEGRAM, maxLength: 200 }, errorMessage.TELEGRAM);
   const [animalTypes, setAnimalTypes] = useState([]);
@@ -26,8 +34,19 @@ const ShelterStep = ({ handleBack, setShelter }) => {
   const vkGroup = useInput('', { regex: regex.VKONTAKTE, maxLength: 200 }, errorMessage.VKONTAKTE);
   const description = useInput('', { notEmpty: true, maxLength: 3000, regex: regex.TEXT }, errorMessage.DESCRIPTION);
   const [isChecked, setIsChecked] = useState(false);
-  const isInvalid = startTime.invalidText || finishTime.invalidText || shelterName.invalidText || INN.invalidText || webSite.invalidText || telegram.invalidText
-  || address.invalidText || okGroup.invalidText || vkGroup.invalidText || description.invalidText || !isChecked || animalTypes.length === 0;
+  const isInvalid =
+    startTime.invalidText ||
+    finishTime.invalidText ||
+    shelterName.invalidText ||
+    INN.invalidText ||
+    webSite.invalidText ||
+    telegram.invalidText ||
+    address.invalidText ||
+    okGroup.invalidText ||
+    vkGroup.invalidText ||
+    description.invalidText ||
+    !isChecked ||
+    animalTypes.length === 0;
 
   const handleLogo = (e) => {
     const sizeLimit = 5 * 1024 * 1024; // ограничение для размера картинки - 5 МБ
@@ -42,10 +61,24 @@ const ShelterStep = ({ handleBack, setShelter }) => {
     }
   };
 
+  const handleMainPhoto = (e) => {
+    const sizeLimit = 5 * 1024 * 1024; // ограничение для размера картинки - 5 МБ
+    if (e.target.files[0].size < sizeLimit) {
+      const fileReader = new FileReader();
+      fileReader.onload = () => {
+        setMainPhoto(fileReader.result);
+      };
+      fileReader.readAsDataURL(e.target.files[0]);
+    } else {
+      setMainPhoto('');
+    }
+  };
+
   useEffect(() => {
     if (!isInvalid) {
       setShelter({
         logo,
+        profile_image: mainPhoto,
         working_from_hour: startTime.value,
         working_to_hour: finishTime.value,
         name: shelterName.value,
@@ -61,26 +94,42 @@ const ShelterStep = ({ handleBack, setShelter }) => {
     }
   }, [isInvalid]);
 
-  useEffect(() => { // добавить маску для полей времени работы приюта
+  // добавить маску для полей времени работы приюта
+  useEffect(() => {
     const maskOptions = { mask: '00:00' };
-    document.querySelectorAll('.add-shelter-form__time-input').forEach((el) => { IMask(el, maskOptions); });
+    document.querySelectorAll('.add-shelter-form__time-input').forEach((el) => {
+      IMask(el, maskOptions);
+    });
   }, []);
 
   return (
     <>
-      <label className='add-shelter-form__caption'>Логотип приюта</label>
-      <div className='add-shelter-form__photo-block'>
-        <label>
-          <input onChange={handleLogo} type='file' name='logo' accept='.jpg, .jpeg, .png, .bmp' multiple={false} />
-          <img className={`add-shelter-form__logo ${!logo && 'add-shelter-form__logo_hidden'}`} src={logo} alt='' />
-        </label>
+      <div className='add-shelter-form__photos'>
+        <div className='add-shelter-form__photo-block'>
+          <label className='add-shelter-form__photo-label'>Логотип приюта</label>
+          <div className='add-shelter-form__photo-input'>
+            <label>
+              <input onChange={handleLogo} type='file' name='logo' accept='.jpg, .jpeg, .png, .bmp' multiple={false} />
+              <img className={`add-shelter-form__logo ${!logo && 'add-shelter-form__logo_hidden'}`} src={logo} alt='' />
+            </label>
+          </div>
+        </div>
+        <div className='add-shelter-form__photo-block'>
+          <label className='add-shelter-form__photo-label'>Фото приюта</label>
+          <div className='add-shelter-form__photo-input'>
+            <label>
+              <input onChange={handleMainPhoto} type='file' name='mainPhoto' accept='.jpg, .jpeg, .png, .bmp' multiple={false} />
+              <img className={`add-shelter-form__logo ${!mainPhoto && 'add-shelter-form__logo_hidden'}`} src={logo} alt='' />
+            </label>
+          </div>
+        </div>
       </div>
       <label className='add-shelter-form__caption'>Часы приюта*</label>
       <div className='add-shelter-form__clock'>
         <input
           className='add-shelter-form__time-input'
           value={startTime.value}
-          onChange={(e) => { startTime.onChange(e); }}
+          onChange={startTime.onChange}
           onBlur={startTime.onBlur}
           type='tel'
           name='startTime'
@@ -91,7 +140,7 @@ const ShelterStep = ({ handleBack, setShelter }) => {
         <input
           className='add-shelter-form__time-input'
           value={finishTime.value}
-          onChange={(e) => { finishTime.onChange(e); }}
+          onChange={finishTime.onChange}
           onBlur={finishTime.onBlur}
           type='tel'
           name='finishTime'
@@ -99,9 +148,7 @@ const ShelterStep = ({ handleBack, setShelter }) => {
           required
         />
       </div>
-      <p className='add-shelter-form__error'>
-        {(startTime.dirty && startTime.invalidText) ? startTime.invalidText : (finishTime.dirty && finishTime.invalidText)}
-      </p>
+      <p className='add-shelter-form__error'>{startTime.dirty && startTime.invalidText ? startTime.invalidText : finishTime.dirty && finishTime.invalidText}</p>
       <div className='add-shelter-form__flex'>
         <ul className='add-shelter-form__column'>
           <DeclarationInput caption='Название приюта*' inputState={shelterName} type='text' name='shelterName' required />
@@ -120,7 +167,7 @@ const ShelterStep = ({ handleBack, setShelter }) => {
       <textarea
         className='add-shelter-form__textarea'
         value={description.value}
-        onChange={(e) => { description.onChange(e); }}
+        onChange={description.onChange}
         onBlur={description.onBlur}
         name='description'
         required
@@ -128,19 +175,36 @@ const ShelterStep = ({ handleBack, setShelter }) => {
       <p className='add-shelter-form__error'>{description.dirty && description.invalidText}</p>
       <div className='register__privacy'>
         <label className='checkbox__container'>
-          <input className='checkbox__input' name='agreement' type='checkbox' onClick={() => { setIsChecked(!isChecked); }} />
+          <input
+            className='checkbox__input'
+            name='agreement'
+            type='checkbox'
+            onClick={() => {
+              setIsChecked(!isChecked);
+            }}
+          />
           <span className='checkbox' />
         </label>
         <p className='register__text'>
           Я согласен с
-          <Link className='register__link' to='/' target='_blank'> Политикой конфиденциальности </Link>
+          <Link className='register__link' to='/' target='_blank'>
+            {' '}
+            Политикой конфиденциальности{' '}
+          </Link>
           и
-          <Link className='register__link' to='/' target='_blank'> Условиями использования сервиса</Link>
+          <Link className='register__link' to='/' target='_blank'>
+            {' '}
+            Условиями использования сервиса
+          </Link>
         </p>
       </div>
       <div className='add-shelter-form__buttons'>
-        <Button disabled={isInvalid} submit>Добавить приют</Button>
-        <Button theme='transparent' onClick={handleBack}>Назад</Button>
+        <Button disabled={isInvalid} submit>
+          Добавить приют
+        </Button>
+        <Button theme='transparent' onClick={handleBack}>
+          Назад
+        </Button>
       </div>
     </>
   );
