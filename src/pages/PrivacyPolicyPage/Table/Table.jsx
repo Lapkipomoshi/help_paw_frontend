46cafcd03cf2f7e2bc433ce78683e377299e5b10
TatianaP <tanya.email4work@gmail.com>
2023-07-13 13:39:27 +0400

    fix: PR comments

--- src/pages/PrivacyPolicyPage/Table/Table.jsx
+++ src/pages/PrivacyPolicyPage/Table/Table.jsx
@@ -3,20 +3,20 @@ import './Table.scss';
 
 const Table = () => {
   return (
-    <table className='table standard-font_type_small'>
+    <table className='privacy-policy-table standard-font_type_small'>
       <tbody>
         <tr>
-          <td className='table__cell'>Цель обработки</td>
-          <td className='table__cell'>
+          <td className='privacy-policy-table__cell'>Цель обработки</td>
+          <td className='privacy-policy-table__cell'>
             Информирование Пользователя посредством отправки электронных писем и предоставление доступа Пользователю к сервисам, информации и/или материалам,
             содержащимся на веб-сайте
           </td>
         </tr>
 
         <tr>
-          <td className='table__cell'>Персональные данные</td>
-          <td className='table__cell'>
-            <ul className='table__list'>
+          <td className='privacy-policy-table__cell'>Персональные данные</td>
+          <td className='privacy-policy-table__cell'>
+            <ul className='privacy-policy-table__list'>
               <li>фамилия, имя, отчество</li>
               <li>электронный адрес</li>
               <li>номера телефонов</li>
@@ -27,14 +27,14 @@ const Table = () => {
         </tr>
 
         <tr>
-          <td className='table__cell'>Правовые основания</td>
-          <td className='table__cell'>уставные (учредительные) документы Оператора</td>
+          <td className='privacy-policy-table__cell'>Правовые основания</td>
+          <td className='privacy-policy-table__cell'>уставные (учредительные) документы Оператора</td>
         </tr>
 
         <tr>
-          <td className='table__cell'>Виды обработки персональных данных</td>
-          <td className='table__cell'>
-            <ul className='table__list'>
+          <td className='privacy-policy-table__cell'>Виды обработки персональных данных</td>
+          <td className='privacy-policy-table__cell'>
+            <ul className='privacy-policy-table__list'>
               <li>Сбор, запись, систематизация, накопление, хранение, уничтожение и обезличивание персональных данных</li>
               <li>Отправка информационных писем на адрес электронной почты</li>
             </ul>
