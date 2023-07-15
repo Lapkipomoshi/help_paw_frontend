18726a11bf49e69d4056f5142ba86b91434315b1
TatianaP <tanya.email4work@gmail.com>
2023-07-15 18:24:50 +0400

    feat: add scroll to slider

--- src/components/CardsSlider/CardsSlider.jsx
+++ src/components/CardsSlider/CardsSlider.jsx
@@ -1,35 +1,25 @@
 import React, { useRef } from 'react';
 import './CardsSlider.scss';
 
-const CardsSlider = ({ children, listLength, columnGap = 24, cardWidth = 200, isButtonsHidden = false }) => {
-  const slider = useRef(null); // элемент слайдера
+const CardsSlider = ({ children, columnGap, isButtonsHidden = false }) => {
+  const sliderContainerRef = useRef(null);
 
-  let position = 0; // смещение карточек в слайдере при листании
+  function handleLeftArrowClick() {
+    sliderContainerRef.current.scrollBy({ behavior: 'smooth', left: -350 });
+  }
 
-  // показать предыдущие карточки
-  const handlePrev = () => {
-    if (position < 0) position += 200;
-    slider.current.childNodes.forEach((element) => {
-      element.style = `transform: translateX(${position}px)`;
-    });
-  };
-
-  // показать следующие карточки
-  const handleNext = () => {
-    if (position >= -(listLength - 7) * (columnGap + cardWidth)) position -= columnGap + cardWidth;
-    slider.current.childNodes.forEach((element) => {
-      element.style = `transform: translateX(${position}px)`;
-    });
-  };
+  function handleRightArrowClick() {
+    sliderContainerRef.current.scrollBy({ behavior: 'smooth', left: 350 });
+  }
 
   return (
     <>
-      <ul className='cards-slider' ref={slider} style={{ columnGap: `${columnGap}px` }}>
+      <ul className='cards-slider' ref={sliderContainerRef} style={{ columnGap: `${columnGap}px` }}>
         {children}
       </ul>
       <div className={`cards-slider__buttons ${isButtonsHidden && 'cards-slider__buttons_hidden'}`}>
-        <button className='cards-slider__button' type='button' onClick={handlePrev} />
-        <button className='cards-slider__button cards-slider__button_next' type='button' onClick={handleNext} />
+        <button className='cards-slider__button' type='button' onClick={handleLeftArrowClick} />
+        <button className='cards-slider__button cards-slider__button_next' type='button' onClick={handleRightArrowClick} />
       </div>
     </>
   );
