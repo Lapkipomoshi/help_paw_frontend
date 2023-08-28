ad4da2f4664ec83bec998526c3799c69e8b80fc7
TatianaP <tanya.email4work@gmail.com>
2023-08-28 13:31:04 +0400

    feat: add metrics output

--- src/modules/ShelterOwnerStatistics/ShelterOwnerStatistics.jsx
+++ src/modules/ShelterOwnerStatistics/ShelterOwnerStatistics.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import './ShelterOwnerStatistics.scss';
 import ShelterMetricsOverview from '../../components/ShelterMetricsOverview/ShelterMetricsOverview';
 
-// TODO доделать на получение данных с бэка вместо статики, Максим добавит чуть позже поля в shelter, чтобы не делать доп запросы
 // TODO додалеть пути, сейчас эти страницы не сверстаны
+
 const ShelterOwnerStatistics = ({ shelter }) => {
-  console.log(shelter);
+  const metrics = [
+    { title: 'Количество питомцев: ', valueKey: shelter.count_pets, actionText: 'Посмотреть всех питомцев', path: '/' },
+    { title: 'Количество вакансий: ', valueKey: shelter.count_pets, actionText: 'Посмотреть все вакансии', path: '/' },
+    { title: 'Количество тегов: ', valueKey: shelter.count_pets, actionText: 'Посмотреть все теги', path: '/' },
+    { title: 'Количество новостей: ', valueKey: shelter.count_pets, actionText: 'Посмотреть все новостей', path: '/' },
+  ];
   return (
     <div className='shelter-owner-statistic'>
-      <ShelterMetricsOverview title='Количество питомцев: ' metricValue='123' actionText='Посмотреть всех питомцев' path='/' />
-      <ShelterMetricsOverview title='Количество вакансий: ' metricValue='32' actionText='Посмотреть все вакансии' path='/' />
-      <ShelterMetricsOverview title='Количество тегов: ' metricValue='1000' actionText='Посмотреть все теги' path='/' />
-      <ShelterMetricsOverview title='Количество новостей: ' metricValue='666' actionText='Посмотреть все новостей' path='/' />
+      {metrics.map((metric) => {
+        return (
+          <ShelterMetricsOverview key={metric.title} title={metric.title} metricValue={metric.valueKey} path={metric.path} actionText={metric.actionText} />
+        );
+      })}
     </div>
   );
 };
