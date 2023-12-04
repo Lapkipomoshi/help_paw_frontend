53f8858a54537dbeb888ecc00c71f14a466c3011
nikita <nikita845217@yandex.ru>
2023-12-04 11:54:59 +0300

    feat: подключение платежа и правки баг-репортов

--- src/modules/HelpToShelter/HelpToShelter.jsx
+++ src/modules/HelpToShelter/HelpToShelter.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { useOutletContext } from 'react-router-dom';
+import { useOutletContext, useParams } from 'react-router-dom';
 import './HelpToShelter.css';
 import { Button } from '../../ui';
 import DeclarationInput from '../../ui/DeclarationInput/DeclarationInput';
 import * as regex from '../../utils/regex';
 import * as errorMessage from '../../utils/errorMessage';
 import useInput from '../../hooks/useInput';
+import { donateToShelter } from './ApiHelpToShelter';
 
 const HelpToShelter = () => {
+  const { id } = useParams();
   const materialAid = useInput('', { notEmpty: true, maxLength: 12, regex: regex.NUMBER }, errorMessage.DONATION_AMOUNT);
   const { isOwner, isAuth, isShelterOwner } = useOutletContext();
+
+  const handleDonate = async () => {
+    if (materialAid.value !== '') {
+      const donationAmount = parseInt(materialAid.value, 10);
+      try {
+        const paymentConfirmUrl = await donateToShelter(id, donationAmount);
+        window.open(paymentConfirmUrl, '_blank');
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    }
+  };
 
   return (
     <section className='shelter-section help-to-shelter'>
@@ -26,7 +40,13 @@ const HelpToShelter = () => {
               placeholder='1000'
             />
           )}
-          <Button className='help-to-shelter__button' disabled={!isAuth && !isShelterOwner}>Пожертвовать деньги</Button>
+          <Button
+            className='help-to-shelter__button'
+            onClick={handleDonate}
+            disabled={(!isAuth && !isShelterOwner) || materialAid.value === ''}
+          >
+            Пожертвовать деньги
+          </Button>
         </div>
       )}
       <div className='help-to-shelter__nonmat'>
