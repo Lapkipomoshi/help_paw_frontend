341de95cbb8fb626805b3b3bf8a4a21afc6dd1ad
Notsmartname <super9ger@gmail.com>
2024-05-11 15:29:12 +0500

    Кнопка активна для владельцев приютов

--- src/modules/ShelterNews/ShelterNews.jsx
+++ src/modules/ShelterNews/ShelterNews.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useOutletContext, useParams } from 'react-router-dom';
 import './ShelterNews.scss';
 import NewsSection from '../NewsSection/NewsSection';
 import { Button } from '../../ui';
 
 const ShelterNews = () => {
   const { id } = useParams();
+  const { isOwner } = useOutletContext();
 
   return (
     <section className='shelter-section shelter-news'>
       <div className='shelter-section-top'>
         <h2 className='shelter-section__title shelter-news__title'>Новости приюта</h2>
-        <Button className='shelter-section__btn-news' disabled>Предложить новость</Button>
+        <Button
+          className='shelter-section__btn-news'
+          disabled={!isOwner}
+        >Предложить новость</Button>
       </div>
       <NewsSection shelterId={id} />
       <Button className='shelter-section__btn-more' theme='transparent'>Больше новостей</Button>
