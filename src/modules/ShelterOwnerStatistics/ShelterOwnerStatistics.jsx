162defb6fed1d9efe9c45d199a5905f26e3618d5
Notsmartname <super9ger@gmail.com>
2024-05-23 17:51:29 +0500

    Кнопка "Посмотреть новости приюта"

--- src/modules/ShelterOwnerStatistics/ShelterOwnerStatistics.jsx
+++ src/modules/ShelterOwnerStatistics/ShelterOwnerStatistics.jsx
@@ -9,7 +9,7 @@ const ShelterOwnerStatistics = ({ shelter }) => {
     { title: 'Количество питомцев: ', valueKey: shelter.count_pets, actionText: 'Посмотреть всех питомцев', path: '/' },
     { title: 'Количество вакансий: ', valueKey: shelter.count_vacancies, actionText: 'Посмотреть все вакансии', path: `/shelters/${shelter.id}/vacancies` },
     { title: 'Количество тегов: ', valueKey: shelter.count_pets, actionText: 'Посмотреть все теги', path: '/' },
-    { title: 'Количество новостей: ', valueKey: shelter.count_pets, actionText: 'Посмотреть новости приюта', path: '/' },
+    { title: 'Количество новостей: ', valueKey: shelter.count_news, actionText: 'Посмотреть новости приюта', path: `/shelters/${shelter.id}/news` },
   ];
   return (
     <div className='shelter-owner-statistic'>
